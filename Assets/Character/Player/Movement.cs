6809dd5c43efae1eb5b4f87589124ee6f4ed1481
nfwGytautas <gytautas.kk@gmail.com>
2021-02-13 21:08:04 +0200

    Player

---
+++ Assets/Character/Player/Movement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float JumpForce = 1000.0f;
+    public float Speed = 1.0f;
+
+    [Range(0.0f, 1000.0f)]
+    public float ForceClamp = 0.0f;
+
+    public GameObject Ground;
+
+    private Rigidbody2D rBody;
+    private Animator animator;
+    private bool grounded = true;
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collided");
+        if (collision.gameObject == Ground)
+        {
+            grounded = true;
+            animator.SetBool("Landed", true);
+        }
+    }
+
+    void Update()
+    {
+        float hForce = Input.GetAxisRaw("Horizontal") * Speed * Time.deltaTime;
+
+        if (hForce == 0.0f)
+        {
+            rBody.velocity = new Vector2(0, rBody.velocity.y);
+            animator.SetFloat("Speed", 0.0f);
+        } 
+        else
+        {
+            rBody.AddForce(Vector2.right * hForce);
+            animator.SetFloat("Speed", Mathf.Abs(hForce));
+
+            if (hForce > 0.0f)
+            {
+                transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+            } 
+            else if (hForce < 0.0f)
+            {
+                transform.eulerAngles = new Vector3(0.0f, 180.0f, 0.0f);
+            }
+        }
+
+        rBody.velocity = new Vector2(Mathf.Clamp(rBody.velocity.x, ForceClamp * -1, ForceClamp), rBody.velocity.y);
+
+        if (Input.GetKey(KeyCode.W) && grounded)
+        {
+            print("?");
+            rBody.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+            grounded = false;
+            animator.SetBool("Landed", false);
+        }
+
+        print(rBody.velocity.y);
+        animator.SetFloat("YVelocity", rBody.velocity.y);
+    }
+}
---
+++ Assets/World/Parallaxing.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxing : MonoBehaviour
+{
+    // Entities that are affected
+    public Transform[] Backgrounds;
+
+    // Entity Z scale values
+    private float[] BGScales;
+
+    // Parallax movement speed
+    public float Smooth = 1.0f;
+
+    // MainCamera transform and position
+    private Transform camTransform;
+    private Vector3 prevCamPosition;
+
+    // Awake is called before Start()
+    void Awake()
+    {
+        camTransform = Camera.main.transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevCamPosition = camTransform.position;
+        BGScales = new float[Backgrounds.Length];
+
+        for (int i = 0; i < Backgrounds.Length; i++)
+        {
+            BGScales[i] = Backgrounds[i].position.z;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < Backgrounds.Length; i++)
+        {
+            float parallax = (prevCamPosition.x - camTransform.position.x) * BGScales[i];
+            float bgTargetPosX = Backgrounds[i].position.x + parallax;
+            Vector3 bgTargetPos = new Vector3(bgTargetPosX, Backgrounds[i].position.y, Backgrounds[i].position.z);
+            Backgrounds[i].position = Vector3.Lerp(Backgrounds[i].position, bgTargetPos, Smooth * Time.deltaTime);
+        }
+
+        prevCamPosition = camTransform.position;
+    }
+}
