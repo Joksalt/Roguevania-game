671997b2429ffd73abd91aac13f97c5e91d7b972
Joksalt <60965298+Joksalt@users.noreply.github.com>
2021-04-19 22:08:28 +0300

    Shop interaction
    Added shop interaction, opening and closing, by pressing "e" button.

---
+++ Assets/InteractionObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour
+{
+
+    public void DoInteraction()
+    {
+        //Picked up and put in inventory
+        if(gameObject.name == "NPC")
+        {
+            GameObject shop = gameObject.transform.Find("Shop_UI").gameObject;
+            if (shop.activeSelf)
+                shop.SetActive(false);
+            else
+                shop.SetActive(true);
+        }
+        //gameObject.SetActive(false);
+
+        //Turn on Shop UI
+        //GameObject shop = GameObject.Find("Shop_UI");
+        //shop.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInteract.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    //An object thay we currently can interact
+    public GameObject currentInterObj = null;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact") && currentInterObj)
+        {
+            //Do something with the object
+            currentInterObj.SendMessage("DoInteraction");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("interObject"))
+        {
+            Debug.Log(other.name);
+            currentInterObj = other.gameObject;
+
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("interObject"))
+        {
+            if (other.gameObject == currentInterObj)
+            {
+                currentInterObj = null;
+            }
+        }        
+    }
+}
